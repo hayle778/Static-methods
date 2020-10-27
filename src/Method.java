04b0065ef896ff15fcd8e77f3392a461cc6ad7a0
yourUsername <yourEmail@domain.com>
2020-10-26 22:52:01 -0400

    ff

---
+++ src/Method.java
@@ -0,0 +1,22 @@
+public class Method {
+
+//  Change the variable to static because of you write above the main method.
+
+    static int a = 0;
+    static int months = 12;
+
+public static void main(String[]args){
+
+    int b = 1;
+
+
+    System.out.println(a+b+months);
+
+}
+
+
+
+
+
+
+}
